7e3789b076ff06740067fa32c6008a270b0dd561
alow-akter <kironalow453@gmail.com>
2022-09-30 12:27:50 -0400

    local stroage commit

--- src/components/WorkOuts/WorkOuts.js
+++ src/components/WorkOuts/WorkOuts.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Workout from '../Workout/Workout';
 import './WrokOuts.css'
+import Swal from 'sweetalert2'
+
+
 const WorkOuts = () => {
     const [workOuts, setWorkOuts] = useState([])
     const [cart, setCart] = useState([])
@@ -26,14 +29,36 @@ const WorkOuts = () => {
 
 
     const btn = (text) => {
+        localStorage.setItem('minutes', text)
         setBtn(text)
+    }
+    const localData = () => {
+        let firstAdd = {};
+        const getValue = localStorage.getItem('minutes')
+        if (getValue) {
+            firstAdd = JSON.parse(getValue)
+        }
+        else {
+            const setValue = localStorage.setItem('minutes', 0)
+            firstAdd = JSON.stringify(setValue)
+        }
+        btn(firstAdd)
+    }
+
+    const aectiveBtn = () => {
+        Swal.fire(
+            'Good job!',
+            'Your work on this project is impressive!',
+            'success'
+        )
     }
 
     return (
         <>
-            <div className='main-container'>
+            <div onLoad={localData} className='main-container'>
                 <div className="workouts-container">
                     {
+
 
                         workOuts.map(workout => <Workout
                             key={workout.id}
@@ -44,7 +69,6 @@ const WorkOuts = () => {
                 <div className="workout-container">
                     <h3>Kiron khan</h3>
                     <h4>Comilla, Bangladesh</h4>
-                    <h2>cart length{cart.length}</h2>
 
                     <div className='cart-container'>
                         <div>
@@ -82,12 +106,33 @@ const WorkOuts = () => {
                             <h6>{btns}m</h6>
                         </div>
                     </div>
-                    <button className='btn-aective'>Activity Completed</button>
+                    <button onClick={aectiveBtn} className='btn-aective'>Activity Completed</button>
                 </div>
 
             </div>
-            <div>
+            <div className='question-container'>
+                <div className='q-one'>
+                    <h1>What are the differences between props and state ?</h1>
+                    <h3>Props:</h3>
 
+                    <p>The Data is passed from one component to another.
+                        It is Immutable (cannot be modified).
+                        Props can be used with state and functional components.Props are read-only.</p>
+                    <h3>State:</h3>
+
+                    <p>The Data is passed within the component only.It is Mutable ( can be modified).State can be used only with the state components/class component (Before 16.0).State is both read and write.</p>
+                </div>
+                <div className='q-two'>
+                    <h2>How does React work ?</h2>
+                    <p>
+                        ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code
+                    </p>
+
+                </div>
+                <div className='q-three'>
+                    <h2>What else does useEffect do without loading data?</h2>
+                    <p>useEffect, you invoke side effects from within functional components, which is an important concept to understand in the React Hooks era. Working with the side effects invoked by the useEffect Hook may seem cumbersome at first, but you’ll eventually learn everything makes a lot of sense.</p>
+                </div>
             </div>
         </>
 
